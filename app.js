64da250f34feb7080e15e88b483edea059ea4dbc
Nguyen Truong Thin <truongthinnguyen@gmail.com>
2015-03-20 20:14:20 -0700

    change routes

--- app.js
+++ app.js
@@ -10,9 +10,118 @@
  */
 
 var express = require('express'),
-    routes = require('./routes');
+    routes = require('./routes'),
+    cors = require('cors');
 
-var app = module.exports = express.createServer();
+var app = module.exports = express.createServer(
+	/*function( request, response ){
+
+
+	    // When dealing with CORS (Cross-Origin Resource Sharing)
+	    // requests, the client should pass-through its origin (the
+	    // requesting domain). We should either echo that or use *
+	    // if the origin was not passed.
+	    var origin = (request.headers.origin || "*");
+
+
+	    // Check to see if this is a security check by the browser to
+	    // test the availability of the API for the client. If the
+	    // method is OPTIONS, the browser is check to see to see what
+	    // HTTP methods (and properties) have been granted to the
+	    // client.
+	    if (request.method.toUpperCase() === "OPTIONS"){
+
+
+	        // Echo back the Origin (calling domain) so that the
+	        // client is granted access to make subsequent requests
+	        // to the API.
+	        response.writeHead(
+	            "204",
+	            "No Content",
+	            {
+	                "access-control-allow-origin": origin,
+	                "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+	                "access-control-allow-headers": "content-type, accept",
+	                "access-control-max-age": 10, // Seconds.
+	                "content-length": 0
+	            }
+	        );
+
+	        // End the response - we're not sending back any content.
+	        return( response.end() );
+
+
+	    }
+
+
+	    // -------------------------------------------------- //
+	    // -------------------------------------------------- //
+
+
+	    // If we've gotten this far then the incoming request is for
+	    // our API. For this demo, we'll simply be grabbing the
+	    // request body and echoing it back to the client.
+
+
+	    // Create a variable to hold our incoming body. It may be
+	    // sent in chunks, so we'll need to build it up and then
+	    // use it once the request has been closed.
+	    var requestBodyBuffer = [];
+
+	    // Now, bind do the data chunks of the request. Since we are
+	    // in an event-loop (JavaScript), we can be confident that
+	    // none of these events have fired yet (??I think??).
+	    request.on(
+	        "data",
+	        function( chunk ){
+
+	            // Build up our buffer. This chunk of data has
+	            // already been decoded and turned into a string.
+	            requestBodyBuffer.push( chunk );
+
+	        }
+	    );
+
+
+	    // Once all of the request data has been posted to the
+	    // server, the request triggers an End event. At this point,
+	    // we'll know that our body buffer is full.
+	    request.on(
+	        "end",
+	        function(){
+
+	            // Flatten our body buffer to get the request content.
+	            var requestBody = requestBodyBuffer.join( "" );
+
+	            // Create a response body to echo back the incoming
+	            // request.
+	            var responseBody = (
+	                "Thank You For The Cross-Domain AJAX Request:\n\n" +
+	                "Method: " + request.method + "\n\n" +
+	                requestBody
+	            );
+
+	            // Send the headers back. Notice that even though we
+	            // had our OPTIONS request at the top, we still need
+	            // echo back the ORIGIN in order for the request to
+	            // be processed on the client.
+	            response.writeHead(
+	                "200",
+	                "OK",
+	                {
+	                    "access-control-allow-origin": origin,
+	                    "content-type": "text/plain",
+	                    "content-length": responseBody.length
+	                }
+	            );
+
+	            // Close out the response.
+	            return( response.end( responseBody ) );
+
+	        }
+	    );
+	}*/
+);
 
 // Configuration
 
@@ -23,7 +132,10 @@ app.configure(function(){
     app.use(express.methodOverride());
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
+
 });
+
+//app.use(cors({credentials: true}));
 
 app.configure('development', function(){
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -34,13 +146,13 @@ app.configure('production', function(){
 });
 
 // Routes
-
 // index page
 app.get('/', routes.index);
 app.post('/', routes.index_post);
 
 // room page
 app.get('/room', routes.room);
+app.get('/roomDetail', routes.roomDetail);
 app.post('/room', routes.room_post);
 app.get('/room/:id/edit', routes.room_edit);
 app.post('/room/:id/edit', routes.room_update);
--- routes/index.js
+++ routes/index.js
@@ -21,22 +21,149 @@ exports.index = function(req, res){
 };
 
 // Home page => room form
-exports.room = function(req, res){
-    console.log( req.xhr);
+exports.room = function(request, response){
     var tpl = "<a id=\"{{id}}\" class=\"small-room black\" href=\"javascript:void(0)\" data-toggle=\"modal\" data-target=\".modal-{{id}}\">" +
-                    "<div class=\"small-visual\">" +
-                        "<i class=\"fa fa-bed\"></i>" +
-                    "</div>" +
-                    "<div class=\"small-visual-light\">" +
-                        "<i id=\"small-light1\" class=\"fa fa-lightbulb-o\"></i>" +
-                    "</div>" +
+                "<div class=\"small-visual\">" +
+                    "<i class=\"fa fa-bed\"></i>" +
+                "</div>" +
+                "<div class=\"small-visual-light\">" +
+                    "<i id=\"small-light1\" class=\"fa fa-lightbulb-o\"></i>" +
+                "</div>" +
 
-                    "<div class=\"small-roomname\">{{title}}</div>" +
-                    "<div id=\"smallCash\" class=\"small-cash\">--- ---</div>" +
-                    "<div id=\"smallTime\" class=\"small-time\">--- ---</div>" +
-                "</a>";
+                "<div class=\"small-roomname\">{{title}}</div>" +
+                "<div id=\"smallCash\" class=\"small-cash\">--- ---</div>" +
+                "<div id=\"smallTime\" class=\"small-time\">--- ---</div>" +
+            "</a>";
 
     roomModel.find({}, function(err, docs){
+        if(request.headers['referer']){
+            res.render('room.jade', { title: 'Dua Leo Hotel - Room', messages: [], errors: [], rooms: docs });
+        }
+        else{
+            var origin = (request.headers.origin || "*");
+
+            // When dealing with CORS (Cross-Origin Resource Sharing)
+            // requests, the client should pass-through its origin (the
+            // requesting domain). We should either echo that or use *
+            // if the origin was not passed.
+            var origin = (request.headers.origin || "*");
+
+
+            // Check to see if this is a security check by the browser to
+            // test the availability of the API for the client. If the
+            // method is OPTIONS, the browser is check to see to see what
+            // HTTP methods (and properties) have been granted to the
+            // client.
+            if (request.method.toUpperCase() === "OPTIONS"){
+
+
+                // Echo back the Origin (calling domain) so that the
+                // client is granted access to make subsequent requests
+                // to the API.
+                response.writeHead(
+                    "204",
+                    "No Content",
+                    {
+                        "access-control-allow-origin": origin,
+                        "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+                        "access-control-allow-headers": "content-type, accept",
+                        "access-control-max-age": 10, // Seconds.
+                        "content-length": 0
+                    }
+                );
+
+                // End the response - we're not sending back any content.
+                return( response.end() );
+
+
+            }
+
+
+            // -------------------------------------------------- //
+            // -------------------------------------------------- //
+
+
+            // If we've gotten this far then the incoming request is for
+            // our API. For this demo, we'll simply be grabbing the
+            // request body and echoing it back to the client.
+
+
+            // Create a variable to hold our incoming body. It may be
+            // sent in chunks, so we'll need to build it up and then
+            // use it once the request has been closed.
+            var requestBodyBuffer = [];
+
+            // Now, bind do the data chunks of the request. Since we are
+            // in an event-loop (JavaScript), we can be confident that
+            // none of these events have fired yet (??I think??).
+            request.on(
+                "data",
+                function( chunk ){
+                    console.log(chunk);
+                    // Build up our buffer. This chunk of data has
+                    // already been decoded and turned into a string.
+                    requestBodyBuffer.push( chunk );
+
+                }
+            );
+
+
+            // Once all of the request data has been posted to the
+            // server, the request triggers an End event. At this point,
+            // we'll know that our body buffer is full.
+            request.on(
+                "end",
+                function(){
+
+                    // Flatten our body buffer to get the request content.
+                    var requestBody = requestBodyBuffer.join( "" );
+                    var responseBody;
+
+                    // Create a response body to echo back the incoming
+                    // request.
+
+                    if(err){
+                        responseBody = "<p>Có lỗi xảy ra, vui lòng thử lại sau.</p>";
+                    }
+                    if(docs.length){
+                        var html = '';
+                        for(var i = 0; i < docs.length; i++){
+                            var doc = docs[i];
+                            html += tpl.replace(/{{id}}/g, doc._id).replace(/{{title}}/g, doc.title);
+                        }
+                        responseBody = html;
+                    }
+                    else{
+                         responseBody = "<p>Hiện tại khách sạn chưa có phòng nào</p>";
+                    }
+
+                    //console.log(responseBody.length);
+
+                    // Send the headers back. Notice that even though we
+                    // had our OPTIONS request at the top, we still need
+                    // echo back the ORIGIN in order for the request to
+                    // be processed on the client.
+                    response.writeHead(
+                        "200",
+                        "OK",
+                        {
+                            "access-control-allow-origin": origin,
+                            "content-type": "text/html",
+                            "content-length": responseBody.length + 2
+                        }
+                    );
+
+                    // Close out the response.
+                    return( response.end( responseBody ) );
+
+                }
+            );
+        }
+    });
+
+
+
+    /*roomModel.find({}, function(err, docs){
         if(req.xhr){
             if(err){
                 res.end("<p>Có lỗi xảy ra, vui lòng thử lại sau.</p>");
@@ -56,8 +183,232 @@ exports.room = function(req, res){
         else{
             res.render('room.jade', { title: 'Dua Leo Hotel - Room', messages: [], errors: [], rooms: docs });
         }
-    });
+    });*/
 };
+
+exports.roomDetail = function(request, response){
+    if(!request.headers['referer']){
+        // When dealing with CORS (Cross-Origin Resource Sharing)
+        // requests, the client should pass-through its origin (the
+        // requesting domain). We should either echo that or use *
+        // if the origin was not passed.
+        var origin = (request.headers.origin || "*");
+
+
+        // Check to see if this is a security check by the browser to
+        // test the availability of the API for the client. If the
+        // method is OPTIONS, the browser is check to see to see what
+        // HTTP methods (and properties) have been granted to the
+        // client.
+
+        if (request.method.toUpperCase() === "OPTIONS"){
+
+
+            // Echo back the Origin (calling domain) so that the
+            // client is granted access to make subsequent requests
+            // to the API.
+            response.writeHead(
+                "204",
+                "No Content",
+                {
+                    "access-control-allow-origin": origin,
+                    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+                    "access-control-allow-headers": "content-type, accept",
+                    "access-control-max-age": 10, // Seconds.
+                    "content-length": 0
+                }
+            );
+
+            // End the response - we're not sending back any content.
+            return( response.end() );
+
+
+        }
+
+
+        // -------------------------------------------------- //
+        // -------------------------------------------------- //
+
+
+        // If we've gotten this far then the incoming request is for
+        // our API. For this demo, we'll simply be grabbing the
+        // request body and echoing it back to the client.
+
+
+        // Create a variable to hold our incoming body. It may be
+        // sent in chunks, so we'll need to build it up and then
+        // use it once the request has been closed.
+        var requestBodyBuffer = [];
+
+        // Now, bind do the data chunks of the request. Since we are
+        // in an event-loop (JavaScript), we can be confident that
+        // none of these events have fired yet (??I think??).
+        request.on(
+            "data",
+            function( chunk ){
+                // Build up our buffer. This chunk of data has
+                // already been decoded and turned into a string.
+                requestBodyBuffer.push( chunk );
+
+            }
+        );
+
+
+        // Once all of the request data has been posted to the
+        // server, the request triggers an End event. At this point,
+        // we'll know that our body buffer is full.
+        request.on(
+            "end",
+            function(){
+
+                // Flatten our body buffer to get the request content.
+                var requestBody = requestBodyBuffer.join( "" );
+                var responseBody;
+
+                // Create a response body to echo back the incoming
+                // request.
+
+                /*if(err){
+                    responseBody = "<p>Có lỗi xảy ra, vui lòng thử lại sau.</p>";
+                }
+                if(docs.length){
+                    var html = '';
+                    for(var i = 0; i < docs.length; i++){
+                        var doc = docs[i];
+                        html += tpl.replace(/{{id}}/g, doc._id).replace(/{{title}}/g, doc.title);
+                    }
+                    responseBody = html;
+                }
+                else{
+                     responseBody = "<p>Hiện tại khách sạn chưa có phòng nào</p>";
+                }*/
+
+                responseBody = "<div class=\"modal fade modal-{{id}}\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myLargeModalLabel\" aria-hidden=\"true\">" +
+                                    "<div class=\"modal-dialog modal-lg\">" +
+                                        "<div class=\"modal-content\">" +
+                                            "<p>Hello World</p>" +
+                                        "</div>" +
+                                    "</div>" +
+                                "</div>";
+
+                console.log(responseBody);
+
+                // Send the headers back. Notice that even though we
+                // had our OPTIONS request at the top, we still need
+                // echo back the ORIGIN in order for the request to
+                // be processed on the client.
+                response.writeHead(
+                    "200",
+                    "OK",
+                    {
+                        "access-control-allow-origin": origin,
+                        "content-type": "text/html",
+                        "content-length": responseBody.length + 2
+                    }
+                );
+
+                // Close out the response.
+                return( response.end( responseBody ) );
+
+            }
+        );
+    }
+
+    /*if(req.xhr){
+        var html = '<div class="modal fade modal-{{id}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-lg">
+                            <div class="modal-content">
+                                <div class="recent-post-title widget-body blue black" id="room-{{id}}">
+                                    <div class="visual">
+                                        <i class="fa fa-bed"></i>
+                                    </div>
+                                    <div class="visual">
+                                        <i class="fa fa-bed"></i>
+                                    </div>
+
+                                    <div class="room-no-cont">
+                                        <span class="circle">
+                                                <a onclick="main()" class="power icon-play" id="icon-play"><i
+                                                        class="fa fa-play check-in"></i></a>
+                                                <a onclick="abortTimer()" class="power icon-stop" id="icon-stop"><i
+                                                        class="fa fa-stop check-out"></i></a>
+                                            </span>
+
+                                        <div class="fa room-no">{{title}}</div>
+                                    </div>
+                                    <div class="control">
+                                        <h2>
+                                            Giờ vào: <i id="inHour">--- ---</i>
+                                            <br>
+                                            Giờ ra: <i id="outHour">--- ---</i>
+                                        </h2>
+
+                                        <div class="room-type-group">
+                                            <input type="checkbox" id="overnight" value="1" />
+                                            <label for="overnight">Qua đêm &nbsp</label>
+                                            <input type="radio" name="roomType" id="radio1" class="css-checkbox" checked="checked" value="0">
+                                            <label for="radio1" class="css-label">Quạt &nbsp</label>
+                                            <input type="radio" name="roomType" id="radio2" class="css-checkbox" value="1">
+                                            <label for="radio2" class="css-label">Lạnh</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="food">
+                                        <div class="suoi-group">
+                                            <div class="suoi-control">
+                                                <div class="in-re">
+                                                    <a id="up" class="nuoc-btn btn-up" href="#" onclick="updateSpinnerSuoi(this); return false;">+</a>
+                                                    <a id="down" class="nuoc-btn btn-down" href="#" onclick="updateSpinnerSuoi(this); return false;">-</a>
+                                                </div>
+                                                <input class="suoi" id="suoi" value="0" type="text" />
+                                            </div>
+                                            <span class="suoi-txt">Suối</span>
+                                        </div>
+
+                                        <div class="ngot-group">
+                                            <div class="suoi-control">
+                                                <div class="in-re">
+                                                    <a id="up" class="nuoc-btn btn-up" href="#" onclick="updateSpinnerNgot(this); return false;">+</a>
+                                                    <a id="down" class="nuoc-btn btn-down" href="#" onclick="updateSpinnerNgot(this); return false;">-</a>
+                                                </div>
+                                                <input class="suoi" id="ngot" value="0" type="text" />
+
+                                            </div>
+                                            <span class="suoi-txt">Nước ngọt</span>
+                                        </div>
+
+                                        <div class="mi-group">
+                                            <div class="suoi-control">
+                                                <div class="in-re">
+                                                    <a id="up" class="nuoc-btn btn-up" href="#" onclick="updateSpinnerMi(this); return false;">+</a>
+                                                    <a id="down" class="nuoc-btn btn-down" href="#" onclick="updateSpinnerMi(this); return false;">-</a>
+                                                </div>
+                                                <input class="suoi" id="mi" value="0" type="text" />
+
+                                            </div>
+                                            <span class="suoi-txt">0,000</span>
+                                        </div>
+                                    </div>
+
+                                    <h2 class="cash">Thu:&nbsp<i id="money">--- ---</i> VND</h2>
+
+                                    <div class="info-block">
+                                        <p><i id="icon-clock" class="fa fa-clock-o"></i><b id="totalTime">--- ---</b></p>
+
+                                        <p>
+                                            <i id="person-name" class="fa fa-user"></i>
+                                            <input id="name" class="name" type="text" name="Name" onClick="this.select();" value="Nhập tên và địa chỉ">
+                                            </br>
+                                            <i id="person-name1" class="fa fa-user"></i>
+                                            <input id="name1" class="name" type="text" name="Name1" onClick="this.select();" value="Nhập tên và địa chỉ">
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>'
+    }*/
+}
 
 /*// Member list page
 exports.list = function(req, res){
@@ -84,6 +435,7 @@ exports.csv = function(req, res){
     });
 };
 */
+
 // Member register logic
 exports.room_edit = function(req, res){
 	roomModel.findOne({ _id: req.params.id }, function(err, doc){
@@ -115,7 +467,6 @@ exports.room_update = function(req, res){
 
 // Member register logic
 exports.room_post = function(req, res){
-	console.log(req);
     room = new roomModel();
     //console.log(req.body);
     room.title = req.body.title;
